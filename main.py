a2982b0cebdf230210ee5395faae9baa1797478f
Carlos Silva <ceskodes@gmail.com>
2024-03-07 19:18:46 -0400

    Se cambio idioma a inglés y se mejoraron las opciones en los condicionales

--- main.py
+++ main.py
@@ -1,33 +1,42 @@
 import random
 
-# Opciones del juego.
-options = ("piedra", "papel", "tijera")
-opt = "si"
+# Game options.
+options = ("rock", "paper", "scissors")
+opt = "yes"
 
-while opt == "si":
-    # Entrada de datos.
-    user_option = input("¿Piedra, papel o tijera?:").lower()
-
-    # Computadora selecciona de forma aleatoria la opción.
+while opt == "yes":
+    # Option input.
+    print("*"*100)
+    user_option = input("What do you choose rock, paper or scissors?: ").lower()
+    # Pick a random option and is assigned to computer_option.
     computer_option = random.choice(options)
 
+    # Game results
     if user_option == computer_option:
-        print("Empate!")
-    elif user_option == "piedra":
-        if computer_option == "tijera":
-            print("Piedra gana a tijera!")
+        print(f'User: {user_option} vs Computer: {computer_option}.\n')
+        print("Draw!")
+
+    elif user_option == options[0]:
+        if computer_option == options[2]:
+            print(f'User: {user_option} vs Computer: {computer_option}.\n')
+            print("You win!")
         else:
-            print("Perdiste! el oponente tiene papel")
-    elif user_option == "tijera":
-        if computer_option == "papel":
-            print("Tijera gana a papel, ganaste!")
+            print(f'User: {user_option} vs Computer: {computer_option}.\n')
+            print(f"Your opponent had {computer_option}, therefore you lost!")
+    elif user_option == options[1]:
+        if computer_option == options[0]:
+            print(f'User: {user_option} vs Computer: {computer_option}.\n')
+            print("You win!")
         else:
-            print("Perdiste! el oponente saco piedra")
-    elif user_option == "papel":
-        if computer_option == "piedra":
-            print("Papel le gana a piedra, ganaste!")
+            print(f'User: {user_option} vs Computer: {computer_option}.\n')
+            print(f"Your opponent had {computer_option}, therefore you lost!")
+    elif user_option == options[2]:
+        if computer_option == options[1]:
+            print(f'User: {user_option} vs Computer: {computer_option}.\n')
+            print("You win!")
         else:
-            print("Perdiste! el oponente saco tijeras!")
+            print(f'User: {user_option} vs Computer: {computer_option}.\n')
+            print(f"Your opponent had {computer_option}, therefore you lost!")
     else:
-        print("Opcion invalida")
-    opt = input(f'¿Desea volver a jugar? (Si/No): ').lower()
+        print("Invalid option")
+    opt = input(f'¿Do you want to play again? (Yes/No): ').lower()
