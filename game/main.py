172f8632c15c48aca6b70966c1606a40e09d2b40
Carlos <ceskodes@gmail.com>
2024-06-06 16:28:46 -0400

    Small changes

--- game/main.py
+++ game/main.py
@@ -1,13 +1,15 @@
 import random
-import print_options as po
+from resource import print_options as po
 
-def pick_option(options):
+# Option input (USER) and selection (COMPUTER)
+def pick_option(options: str):
     print("*"*100)
     user_option = input("What do you choose rock, paper or scissors?: ").lower()
     computer_option = random.choice(options)
     return user_option, computer_option
 
-def game_rules(user_option, computer_option, options):
+def game_rules(user_option: str, computer_option: str, options: str):
+    # Same option
     if user_option == computer_option:
         if user_option == options[0]:
             po.user_print_rock()
@@ -21,6 +23,8 @@ def game_rules(user_option, computer_option, options):
             po.user_print_scissors()
             print((" "*25)+"It's a draw!\n")
             po.comp_print_scissors()
+            
+    # Rock vs Scissors (WIN) / Paper (LOSE)        
     elif user_option == options[0]:
         if computer_option == options[2]:
             po.user_print_rock()
@@ -32,6 +36,8 @@ def game_rules(user_option, computer_option, options):
             print((" "*25)+"You lost!\n")
             po.comp_print_paper()
             return "c"
+    
+    # Paper vs Rock (WIN) / Scissors (LOSE)    
     elif user_option == options[1]:
         if computer_option == options[0]:
             po.user_print_paper()
@@ -43,6 +49,8 @@ def game_rules(user_option, computer_option, options):
             print((" "*25)+"You lost!\n")
             po.comp_print_scissors()
             return "c"
+        
+    # Scissors vs Paper (WIN) / Rock (LOSE)    
     elif user_option == options[2]:
         if computer_option == options[1]:
             po.user_print_scissors()
@@ -60,10 +68,10 @@ def game_rules(user_option, computer_option, options):
 def run_game():
     opt = "yes"
     options = ("rock", "paper", "scissors")
-    while opt == "yes":
-        user_score = 0
-        comp_score = 0
-        total_games = input("How many win should the winner have?: ")
+    while opt in {"yes", "y", "ye"}:
+        user_score: int = 0
+        comp_score: int = 0
+        total_games: int = int(input("How many win should the winner have?: "))
         while (user_score != total_games) and (comp_score != total_games):
             user_option, computer_option = pick_option(options)
             if user_option not in options:
@@ -76,5 +84,5 @@ def run_game():
             print("\n"+(" "*27)+f'{user_score} - {comp_score}')
         opt = input(f'Do you want to play again? (Yes/No): ').lower()
 
-run_game()
-# Game options.
+if __name__ == '__main__':
+    run_game()
